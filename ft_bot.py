244072fa04381df032eb53ecd0a1dcb70c8bd73f
AkitotheExiled <myguestmike@gmail.com>
2019-10-30 16:12:30 -0700

    V1.21

--- ft_bot.py
+++ ft_bot.py
@@ -6,6 +6,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 import time
 
+
 class Tracker_Bot():
 
     def __init__(self):
@@ -22,15 +23,13 @@ class Tracker_Bot():
         self.token = ""
         self.t_type = ""
         self.reddit = praw.Reddit(client_id=self.client,
-                             client_secret=self.secret,
-                             password=self.password,
-                             user_agent=self.user_agent,
-                             username=self.user)
+                                  client_secret=self.secret,
+                                  password=self.password,
+                                  user_agent=self.user_agent,
+                                  username=self.user)
         self.flair = CONFIG.get('main', 'SEARCH_FLAIR')
         self.authors = []
         self.debug = bool(CONFIG.getboolean('main', 'DEBUG'))
-
-
 
     def get_token(self):
         client_auth = requests.auth.HTTPBasicAuth(self.client, self.secret)
@@ -42,67 +41,73 @@ class Tracker_Bot():
         self.t_type = response2.json()['token_type']
 
     def find_users(self):
-        for post in self.reddit.subreddit(self.subreddit).search(query='flair:' + self.flair):
-            if post.author.name not in self.authors:
-                self.authors.append(post.author.name)
-        return self.authors
+        try:
+            for post in self.reddit.subreddit(self.subreddit).search(query='flair:' + self.flair):
+                if post.author.name not in self.authors:
+                    self.authors.append(post.author.name)
+            return self.authors
+        except Exception as e:
+            print(e)
 
     def find_users_tthread(self, user):
-        user_info = {user: [{'t': None, 'p': None, 'n': None, 'n-': None}]}
-        for post in self.reddit.subreddit(self.subreddit).search(query='flair:' + self.flair):
-            if post.archived:
-                continue
-            if post.author.name == user:
-                if post.comments:
-                    for comment in post.comments:
-                        if comment.author == self.user and not comment.stickied:
-                            comment2 = self.reddit.comment(comment.id)
-                            comment2.mod.distinguish(how='yes', sticky=True)
-                        if comment.author != self.user and not comment.stickied:
-                            if self.reddit.submission(post.id).locked:
-                                self.reddit.submission(post.id).mod.unlock()
-                            def_com = '|Feedback|0||\n|:-|:-|:-|\n|Positive 0|Neutral 0|Negative 0|'
-                            post.reply(def_com)
+        try:
+            user_info = {user: [{'t': None, 'p': None, 'n': None, 'n-': None}]}
+            for post in self.reddit.subreddit(self.subreddit).search(query='flair:' + self.flair):
+                if post.archived:
+                    continue
+                if post.author.name == user:
+                    if post.comments:
+                        for comment in post.comments:
+                            if comment.author == self.user and not comment.stickied:
+                                comment2 = self.reddit.comment(comment.id)
+                                comment2.mod.distinguish(how='yes', sticky=True)
+                            if comment.author != self.user and not comment.stickied:
+                                if self.reddit.submission(post.id).locked:
+                                    self.reddit.submission(post.id).mod.unlock()
+                                def_com = '|Feedback|0||\n|:-|:-|:-|\n|Positive 0|Neutral 0|Negative 0|'
+                                post.reply(def_com)
+                                comment.refresh()
+                                if not self.reddit.submission(post.id).locked:
+                                    self.reddit.submission(post.id).mod.lock()
+                                continue
+                            if comment.author == self.user and comment.stickied:
+                                regex_search = re.compile(r'\d+').findall(comment.body)
+                                for item in regex_search:
+                                    if item is None:
+                                        regex_search[item] = 0
+                                try:
+                                    user_info[user][0]['t'] = regex_search[0]
+                                    user_info[user][0]['p'] = regex_search[1]
+                                    user_info[user][0]['n'] = regex_search[2]
+                                    user_info[user][0]['n-'] = regex_search[3]
+                                except Exception as e:
+                                    print(e)
+                                return user_info
+                    elif not post.comments:
+                        def_com = '|Feedback|0||\n|:-|:-|:-|\n|Positive 0|Neutral 0|Negative 0|'
+                        post.reply(def_com)
+                        for comment in post.comments:
                             comment.refresh()
-                            if not self.reddit.submission(post.id).locked:
-                                self.reddit.submission(post.id).mod.lock()
-                            continue
-                        if comment.author == self.user and comment.stickied:
-                            regex_search = re.compile(r'\d+').findall(comment.body)
-                            for item in regex_search:
-                                if item is None:
-                                    regex_search[item] = 0
-                            try:
-                                user_info[user][0]['t'] = regex_search[0]
-                                user_info[user][0]['p'] = regex_search[1]
-                                user_info[user][0]['n'] = regex_search[2]
-                                user_info[user][0]['n-'] = regex_search[3]
-                            except Exception as e:
-                                print(e)
-                            return user_info
-                elif not post.comments:
-                    def_com = '|Feedback|0||\n|:-|:-|:-|\n|Positive 0|Neutral 0|Negative 0|'
-                    post.reply(def_com)
-                    for comment in post.comments:
-                        comment.refresh()
-                        if comment.author == self.user and not comment.stickied:
-                            comment2 = self.reddit.comment(comment.id)
-                            comment2.mod.distinguish(how='yes', sticky=True)
-                        if comment.author == self.user and comment.stickied:
-                            regex_search = re.compile(r'\d+').findall(comment.body)
-                            for item in regex_search:
-                                if item is None:
-                                    regex_search[item] = 0
-                            try:
-                                user_info[user][0]['t'] = regex_search[0]
-                                user_info[user][0]['p'] = regex_search[1]
-                                user_info[user][0]['n'] = regex_search[2]
-                                user_info[user][0]['n-'] = regex_search[3]
-                            except Exception as e:
-                                print(e)
-                            if not self.reddit.submission(post.id).locked:
-                                self.reddit.submission(post.id).mod.lock()
-                            return user_info
+                            if comment.author == self.user and not comment.stickied:
+                                comment2 = self.reddit.comment(comment.id)
+                                comment2.mod.distinguish(how='yes', sticky=True)
+                            if comment.author == self.user and comment.stickied:
+                                regex_search = re.compile(r'\d+').findall(comment.body)
+                                for item in regex_search:
+                                    if item is None:
+                                        regex_search[item] = 0
+                                try:
+                                    user_info[user][0]['t'] = regex_search[0]
+                                    user_info[user][0]['p'] = regex_search[1]
+                                    user_info[user][0]['n'] = regex_search[2]
+                                    user_info[user][0]['n-'] = regex_search[3]
+                                except Exception as e:
+                                    print(e)
+                                if not self.reddit.submission(post.id).locked:
+                                    self.reddit.submission(post.id).mod.lock()
+                                return user_info
+        except Exception as e:
+            print(e)
 
     def search_for_feedback(self, user):
         try:
@@ -121,12 +126,12 @@ class Tracker_Bot():
         except Exception as e:
             print(e)
 
-    def findall(self, p,s):
+    def findall(self, p, s):
         '''FOR DEBUGGING PURPOSES'''
         i = s.find(p)
         while i != -1:
             yield i
-            i = s.find(p, i+1)
+            i = s.find(p, i + 1)
 
     def update_user_feedback(self, user):
         old_info = self.find_users_tthread(user)
@@ -135,7 +140,8 @@ class Tracker_Bot():
             new_info = {user: [{'t': 0, 'p': 0, 'p': 0, 'n': 0, 'n-': 0}]}
         if old_info is None:
             old_info = {user: [{'t': 0, 'p': 0, 'p': 0, 'n': 0, 'n-': 0}]}
-        if int(old_info[user][0]['t']) != new_info[user][0]['t'] or int(old_info[user][0]['n']) != new_info[user][0]['n']:
+        if int(old_info[user][0]['t']) != new_info[user][0]['t'] or int(old_info[user][0]['n']) != new_info[user][0][
+            'n']:
             print(user + "'s" + " feedback is updating...")
             print('The bot will automatically update the information for you.')
             com_t = new_info[user][0]['t']
@@ -169,16 +175,18 @@ class Tracker_Bot():
                                 # body2[match.start():match.end()] = str(com_ne)
                             # body2 = "".join(body2)
 
-                        body2 = '|Feedback|'+str(com_t)+'||\n|:-|:-|:-|\n|Positive '+str(com_p)+'|Neutral '+str(com_n)+'|Negative '+str(com_ne)+'|'
+                        body2 = '|Feedback|' + str(com_t) + '||\n|:-|:-|:-|\n|Positive ' + str(
+                            com_p) + '|Neutral ' + str(com_n) + '|Negative ' + str(com_ne) + '|'
                         comment.edit(body2)
-                
 
         if int(old_info[user][0]['t']) == new_info[user][0]['t']:
             return print(str(user) + ' has no new feedback.')
+
     def batch_update(self):
         users = self.find_users()
         for user in users:
             self.update_user_feedback(user)
+
 
 if __name__ == '__main__':
     logging.basicConfig()
@@ -186,9 +194,6 @@ if __name__ == '__main__':
     bot = Tracker_Bot()
     scheduler = BackgroundScheduler()
     scheduler.start()
-    scheduler.add_job(bot.batch_update,'interval', id='batch_id_001', seconds=2)
+    scheduler.add_job(bot.batch_update, 'interval', id='batch_id_001', minutes=2)
     input('The bot is running in the background. Press enter to exit.')
     scheduler.shutdown()
-
-
-
